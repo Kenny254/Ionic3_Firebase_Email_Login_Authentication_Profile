c879e6ca3af55546437c912536e1a0a80674c284
patrickkabongo <cracciska@gmail.com>
2017-09-22 15:31:02 -0300

    first commit

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBwP5ttOFLrzS-VRvhDwzS8G7OL_WZhYYQ",
+    authDomain: "fir-auth-1024a.firebaseapp.com",
+    databaseURL: "https://fir-auth-1024a.firebaseio.com",
+    projectId: "fir-auth-1024a",
+    storageBucket: "fir-auth-1024a.appspot.com",
+    messagingSenderId: "556430312821"
+  };
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { AngularFireAuth } from "angularfire2/auth";
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
+import { Profile } from '../../models/profile';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  profileData: FirebaseObjectObservable<Profile>
+
+  constructor(private afAuth: AngularFireAuth, private toast: ToastController,
+    private afDatabase: AngularFireDatabase,public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.afAuth.authState.subscribe(data => {
+      if (data && data.email && data.uid) {
+        this.toast.create({
+          message: `Welcome to APP_NAME, ${data.email}`,
+          duration: 3000
+        }).present();
+
+        this.profileData = this.afDatabase.object(`profile/${data.uid}`)
+      }
+      else {
+        this.toast.create({
+          message: `Could not find authentication detail`,
+          duration: 3000
+        }).present();
+      }
+    });
 
   }
 
